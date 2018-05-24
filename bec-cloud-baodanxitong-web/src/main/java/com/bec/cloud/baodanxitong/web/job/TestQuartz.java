035ca30a6bac131249844e6ec58373a3465729fd
suruiliang <suruiliang_1988@163.com>
2018-05-24 10:47:21 +0800

    repository

--- bec-cloud-baodanxitong-web/src/main/java/com/bec/cloud/baodanxitong/web/job/TestQuartz.java
+++ bec-cloud-baodanxitong-web/src/main/java/com/bec/cloud/baodanxitong/web/job/TestQuartz.java
@@ -9,7 +9,7 @@ import org.springframework.scheduling.quartz.QuartzJobBean;
 /**
 * @author suruiliang
 * @version 创建时间：2018年5月23日 上午11:50:29
-* @ClassName 类名称
+* @ClassName 类名称:com.bec.cloud.baodanxitong.web.job.TestQuartz
 * @Description 类描述
 */
 public class TestQuartz extends QuartzJobBean {
