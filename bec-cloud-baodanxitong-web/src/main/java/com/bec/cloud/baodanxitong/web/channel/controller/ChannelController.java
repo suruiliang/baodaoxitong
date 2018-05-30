1525b25f40ff36bc0ac2d4346a3ea97561d73f2d
wxb <934804229@qq.com>
2018-05-30 11:59:22 +0800

    bec-cloud-baodanxitong-web/src/main/resources/mapper/channel/ChannelMapper.xml

--- bec-cloud-baodanxitong-web/src/main/java/com/bec/cloud/baodanxitong/web/channel/controller/ChannelController.java
+++ bec-cloud-baodanxitong-web/src/main/java/com/bec/cloud/baodanxitong/web/channel/controller/ChannelController.java
@@ -1,14 +1,25 @@
 package com.bec.cloud.baodanxitong.web.channel.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.POST;
+
+import org.apache.poi.ss.formula.functions.T;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bec.cloud.auth.core.support.Result;
+import com.bec.cloud.baodanxitong.web.channel.model.Channel;
 import com.bec.cloud.baodanxitong.web.channel.service.ChannelService;
+import com.bec.cloud.baodanxitong.web.util.PageUtil;
+import com.bec.cloud.service.example.utils.CheckParamUtil;
 
 /**
  * 
- * @author niwei
+ * @author wxb
  * @time   2018下午4:09:00
  * @description
  */
@@ -18,5 +29,63 @@ public class ChannelController {
 	
 	@Autowired
 	private ChannelService channelService;
-
+	
+	/**
+	 * 查询通道信息
+	 * @param request
+	 * @param record
+	 * @return	list
+	 */
+	@GetMapping(value = "/list")
+	public Result<?> list(HttpServletRequest request,Channel record){
+		PageUtil.startPage(request);
+		return Result.success(channelService.selectChannel(record));
+	}
+	/**
+	 * 查询通道信息
+	 * @param channelId
+	 * @return 单条
+	 */
+	@GetMapping(value = "/view/{channelId}")
+	public Result<?> view(@PathVariable Integer channelId){
+		return Result.success(channelService.selectByChannelId(channelId));
+	}
+	/**
+	 * 新增通道
+	 * @param record
+	 * @return
+	 */
+	@PostMapping(value = "/save")
+	public Result<?> svae(Channel record){
+		CheckParamUtil.checkParam(record, "channelCode","channelName","channelUrl","channelRemark","channelStatus","delStatus");
+		return Result.success(channelService.insertSelective(record));
+	}
+	/**
+	 * 逻辑删除单条通道信息
+	 * @param channelId
+	 * @return
+	 */
+	@PostMapping(value = "/del/{channelId,channelRemark}")
+	public Result<?> del(@PathVariable Integer channelId,@PathVariable String channelRemark){
+		return Result.success(channelService.delByChannelId(channelId,channelRemark));
+	}
+	/**
+	 * 逻辑批量删除通道信息
+	 * @param channelIds
+	 * @return
+	 */
+	@PostMapping(value = "/delbatch")
+	public Result<?> delbatch(String channelIds,@PathVariable String channelRemark){
+		return Result.success(channelService.delByChannelId(channelIds,channelRemark));
+	}
+	/**
+	 * 更新通道信息
+	 * @param record
+	 * @return
+	 */
+	@PostMapping(value = "/update")
+	public Result<?> update(Channel record){
+		CheckParamUtil.checkParam(record, "channelCode","channelName","channelUrl","channelRemark","channelStatus");
+		return Result.success(channelService.updateByChannelId(record));
+	}
 }
